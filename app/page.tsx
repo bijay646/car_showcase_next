5c553e612dd613c508992579c38f909e1529eacc
bijay646 <bijaylimbu1997@gmail.com>
2023-12-18 23:46:10 +0545

    change card layout add titles and image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CustomFilter, Hero, SearchBar } from "@/components";
+import CarCard from "@/components/CarCard";
 import { featchCars } from "@/utils";
 import Image from "next/image";
 
@@ -22,7 +23,13 @@ export default async function Home() {
           </div>
         </div>
         {!isDataEmpty ? (
-          <section>WE HAVE CARS</section>
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car}/>
+              ))}
+            </div>
+          </section>
         ) : (
           <div className="home__error-container">
             <h2 className="text-black text-xl font-bold">Oops, no results</h2>
